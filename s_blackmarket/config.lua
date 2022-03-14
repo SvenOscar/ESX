69e4a7b459b01e7d41fd375c29bf09966392616a
Sven <44802975+SvenOscar@users.noreply.github.com>
2022-03-14 11:13:16 +0100

    fix(added strings for easy translating)

--- s_blackmarket/config.lua
+++ s_blackmarket/config.lua
@@ -4,6 +4,11 @@ Config.DrawDistance = 5.0
 Config.drawmarker = true
 
 --If you wish to have additional blackmarkets, just add a new line down below.
-Config.BlackMarkets = {
+Config["Blackmarkets"] = {
    vector3(83.87, 3718.72, 39.75),
+}
+
+Config["Strings"] = {
+   ["openMarket"] = "Press ~INPUT_CONTEXT~ to open the blackmarket.",
+   ["notCash"] = "You ~r~don't~s~ have enough cash!"
 }
